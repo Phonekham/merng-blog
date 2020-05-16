a59bb4cd141326e1bc9399ef4a5f9b7d291afb1b
Phonekham <phone.madridista@gmail.com>
2020-05-16 11:41:57 +0700

    register form

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container">
       <div className="row p-5">
         <h4>Login</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              placeholder="Enter email"
+              disabled={loading}
+            ></input>
+          </div>
+          <button
+            className="btn btn-raised btn-primary"
+            disabled={!email || loading}
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="container">
-      <div className="row p-5">
-        <h4>Register</h4>
-      </div>
+      <h4>Register</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            placeholder="Enter email"
+            disabled={loading}
+          ></input>
+        </div>
+        <button
+          className="btn btn-raised btn-primary"
+          disabled={!email || loading}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
