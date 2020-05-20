30dfd6a5d15780dfc8526c2ba528318f3cc10806
Phonekham <phone.madridista@gmail.com>
2020-05-20 11:39:46 +0700

    reuse auth form

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 
 import { auth } from "../../firebase";
+import AuthForm from "../../components/forms/AuthForm";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -23,31 +24,18 @@ const Register = () => {
     setLoading(false);
   };
   return (
-    <div className="container">
+    <div className="container p-5">
       {loading ? (
         <h4 className="text-danger">Loading...</h4>
       ) : (
         <h4>Register</h4>
       )}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email Address</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control"
-            placeholder="Enter email"
-            disabled={loading}
-          ></input>
-        </div>
-        <button
-          className="btn btn-raised btn-primary"
-          disabled={!email || loading}
-        >
-          Submit
-        </button>
-      </form>
+      <AuthForm
+        email={email}
+        loading={loading}
+        setEmail={setEmail}
+        handleSubmit={handleSubmit}
+      ></AuthForm>
     </div>
   );
 };
