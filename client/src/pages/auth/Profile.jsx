2e0d259d045674d50accf13a48a11956108cf176
Phonekham <phone.madridista@gmail.com>
2020-05-23 11:36:42 +0700

    profile update form

--- client/src/pages/auth/Profile.jsx
+++ client/src/pages/auth/Profile.jsx
@@ -47,7 +47,85 @@ const Profile = () => {
     }
   }, [data]);
 
-  return <div className="container p-5">{JSON.stringify(data)}</div>;
+  // destructure
+  const { username, name, about, email } = values;
+
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+
+  const handleImageChange = () => {};
+
+  const profileUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+          className="form-control"
+          placeholder="username"
+          disabled={loading}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          className="form-control"
+          placeholder="name"
+          disabled={loading}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          className="form-control"
+          placeholder="email"
+          disabled={loading}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">image</label>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          className="form-control"
+          placeholder="image"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">about</label>
+        <textarea
+          name="about"
+          value={username}
+          onChange={handleChange}
+          className="form-control"
+          placeholder="about"
+          disabled={loading}
+        />
+      </div>
+      <button
+        className="btn btn-primary"
+        type="submit"
+        disabled={!email || loading}
+      >
+        Submit
+      </button>
+    </form>
+  );
+
+  return <div className="container p-5">{profileUpdateForm()}</div>;
 };
 
 export default Profile;
