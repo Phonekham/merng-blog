1c071155469661cdbd62e72a313f4ab858bf88e7
Phonekham <phone.madridista@gmail.com>
2020-05-25 13:07:25 +0700

    profile query using route params

--- client/src/App.js
+++ client/src/App.js
@@ -57,7 +57,7 @@ const App = () => {
         />
         <PrivateRoute exact path="/profile" component={Profile} />
         <PrivateRoute exact path="/post/create" component={Post} />
-        <Route exact path="/user" component={SingleUser} />
+        <Route exact path="/user/:username" component={SingleUser} />
       </Switch>
     </ApolloProvider>
   );
--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const UserCard = ({ user }) => {
   const { username, images, about } = user;
@@ -7,7 +8,9 @@ const UserCard = ({ user }) => {
     <div className="card text-center" style={{ minHeight: "375px" }}>
       <div className="card-body">
         <Image image={images[0]}></Image>
-        <h4 className="text-primary">@{username}</h4>
+        <Link to={`/user/${username}`}>
+          <h4 className="text-primary">@{username}</h4>
+        </Link>
         <hr></hr>
         <small>{about}</small>
       </div>
