5705fe824cb250e0921c90ad0cad4b4e5cb08f32
Phonekham <phone.madridista@gmail.com>
2020-05-25 12:18:25 +0700

    user card component

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 
 import { GET_ALL_USERS } from "../graphql/queries";
+import UserCard from "../components/UserCard";
 
 const Users = () => {
   const { data, loading, error } = useQuery(GET_ALL_USERS);
@@ -12,16 +13,9 @@ const Users = () => {
     <div className="container">
       <div className="row pt-5">
         {data &&
-          data.allUsers.map((u) => (
-            <div className="col-md-4" key={u._id}>
-              <div className="card">
-                <div className="card-body">
-                  <div className="card-title">
-                    <h4>{u.username}</h4>
-                  </div>
-                  <p className="card-text">{u.about}</p>
-                </div>
-              </div>
+          data.allUsers.map((user) => (
+            <div className="col-md-4" key={user._id}>
+              <UserCard user={user}></UserCard>
             </div>
           ))}
       </div>
