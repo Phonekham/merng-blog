639c7b0c6c79dcaeb3411e3e99eed6520ccf884c
Phonekham <phone.madridista@gmail.com>
2020-05-26 09:44:17 +0700

    postcreate

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,33 +1,33 @@
 const { gql } = require("apollo-server-express");
 const { posts } = require("../temp");
 const { authCheck } = require("../helpers/auth");
+const User = require("../models/user");
+const Post = require("../models/post");
 
 // queries
 const totalPosts = () => posts.length;
-const allPosts = async (parent, args, { req }) => {
-  await authCheck(req);
-  return posts;
-};
-
 // mutation
-const newPost = (parent, args) => {
-  // console.log(args);
-  // create a new post object
-  const post = {
-    id: posts.length + 1,
+const postCreate = async (parent, args, { req }) => {
+  const currentUser = await authCheck(req);
+
+  // validate
+  if (args.input.content.trim() === "") throw new Error("content is required");
+
+  const currentUserFromDB = await User.findOne({
+    email: currentUser.email,
+  });
+  let newPost = await new Post({
     ...args.input,
-  };
-  // push new post object to posts array
-  posts.push(post);
-  return post;
+    postedBy: currentUserFromDB._id,
+  })
+    .save()
+    .then((post) => post.populate("postedBy", "_id username").execPopulate());
+  return newPost;
 };
 
 module.exports = {
-  Query: {
-    totalPosts,
-    allPosts,
-  },
+  Query: { totalPosts },
   Mutation: {
-    newPost,
+    postCreate,
   },
 };
