18e5a9c4fa72f15768d87a268efc4ee7450b9659
Phonekham <phone.madridista@gmail.com>
2020-05-27 09:50:16 +0700

    create post form

--- client/src/pages/post/Post.jsx
+++ client/src/pages/post/Post.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import omitDeep from "omit-deep";
+
+import { AuthContext } from "../../context/authContext";
+
+const initialState = {
+  content: "",
+  image: {
+    url: "https://via.placeholder.com/200x200.png?text=Post",
+    public_id: "123",
+  },
+};
 
 const Post = () => {
-  return <div>post</div>;
+  const [values, setValues] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const { content } = values;
+
+  const createForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <textarea
+          value={content}
+          onChange={handleChange}
+          name="content"
+          rows="10"
+          className="md-textarea form-control"
+          placeholder="Write something"
+          maxLength="150"
+          disabled={loading}
+        ></textarea>{" "}
+      </div>
+      <button
+        className="btn btn-primary"
+        type="submit"
+        disabled={loading || !content}
+      >
+        Post
+      </button>
+    </form>
+  );
+
+  return (
+    <div className="container p-5">
+      {loading ? <h4 className="text-danger">Loading</h4> : <h4>Create</h4>}
+      {createForm()}
+      {JSON.stringify(content)}
+    </div>
+  );
 };
 
 export default Post;
